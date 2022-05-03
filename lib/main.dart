f47c30057906517c8db37925b3adbd39d4f9fee3
Halil Ücel <hucelsoft@gmail.com>
2022-05-03 08:13:12 +0300

    viseual eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const MyHomePage(title: 'Git Learn Home Page'),
     );
